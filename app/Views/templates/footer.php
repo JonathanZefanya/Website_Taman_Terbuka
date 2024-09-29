dd3126a3669074d275cfb97ee28bd0dda472b066
JonathanZefanya <jonathan.natannael@student.iti.ac.id>
2024-09-29 15:05:52 +0700

    Update map coordinates and improve place descriptions

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -3,7 +3,7 @@
         <div class="flex flex-wrap justify-between items-center">
             <div class="text-center md:text-left">
                 <h3 class="text-lg font-bold">GIS TAMAN TERBUKA</h3>
-                <p class="text-sm mt-2">Menjelajahi keindahan TAMAN TERBUKA</p>
+                <p class="text-sm mt-2">Menjelajahi keindahan taman terbuka</p>
             </div>
             <div class="mt-4 md:mt-0 text-center">
                 <p class="text-sm">Made with <span class="text-orange-500">🧡</span> by <a href="https://jojo.tirtagt.xyz">Web Dev</a></p>
